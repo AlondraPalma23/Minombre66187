3d5651a30be8b10345b6198f6de4644f161c575a
DELL <DELL@DELL-PC>
2021-02-25 20:20:35 -0600

    mi primer comit 1

---
+++ src/minombre66187/Minombre66187.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minombre66187;
+
+/**
+ *
+ * @author Alondra Palma
+ */
+public class Minombre66187 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Alondra Sarai Palma Pacheco");
+    }
+    
+}
